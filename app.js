870c7e962954ef39e3832401f66d4b53eb91a726
mbwolff <wolff.mark.b@gmail.com>
2026-05-27 17:13:07 -0400

    Fixed js after adding comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2026  Mark Wolff <wolff.mark.b@gmail.com>
+/* Copyright (C) 2026  Mark Wolff <wolff.mark.b@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 // This program is free software: you can redistribute it and/or modify
@@ -12,7 +12,7 @@
 // GNU Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
 /* ══════════════════════════════════════════════════════════════════
    app.js — Dictionnaire des idées reçues
